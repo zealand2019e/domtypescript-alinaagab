268f4ea7c04f5cb93886e0f6332e26701cbd0372
alinagabrriela <45002245+alinagabrriela@users.noreply.github.com>
2019-10-03 18:18:53 +0200

    Task 1

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,1 +1,27 @@
 //Her skal du skrive din typescriptkode
+let myElement:HTMLLIElement = <HTMLLIElement> document.getElementById("0");
+console.log(myElement);
+
+if (myElement.hasAttribute("class"))
+{
+    console.log("element has class attribute")
+
+    if (myElement.getAttribute("class") =="usundt")
+    {
+        myElement.setAttribute("class", "sundt");
+    }
+}
+let myList: HTMLCollectionOf<HTMLElement> = <HTMLCollectionOf<HTMLElement>> document.getElementsByClassName("usundt");
+for (var i=0;i<myList.length;i++)
+{
+
+    myList[i].innerHTML="Appelsin";
+}
+console.log(myList);
+
+let myTag : HTMLCollectionOf<HTMLElement> = <HTMLCollectionOf<HTMLElement>> document.getElementsByTagName("li");
+for (var i=0;i<myTag.length;i++)
+{
+     myTag[i].innerHTML = "banana"
+}
+
